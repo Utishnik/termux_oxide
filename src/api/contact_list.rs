e01f6792a0865b20c981c8129d61f1f41d300bc8
Utishnik <yrik.kokoyto.00@bk.ru>
2026-03-21 15:33:33 +0300

    потом допишу

--- src/api/contact_list.rs
+++ src/api/contact_list.rs
@@ -1,16 +1,19 @@
+use super::errors::TermuxError;
+use super::run::async_run_api_cmd;
 use serde::{Deserialize, Serialize};
 use simd_json::{json, prelude::*, serde as simd_serde};
+use smol::process::Command as smol_cmd;
 use std::fs;
 
 static JSON_START_SIZE: usize = 256;
 
-#[derive(Serialize, Deserialize, Default,Debug)]
+#[derive(Serialize, Deserialize, Default, Debug)]
 pub struct Contact {
     name: String,
     number: String,
 }
 
-#[derive(Serialize, Deserialize,Debug)]
+#[derive(Serialize, Deserialize, Debug)]
 #[serde(transparent)]
 pub struct ContactList {
     inner: Vec<Contact>,
@@ -83,7 +86,7 @@ pub fn ser_list_json(list: ContactList, path_file: String) {
     let format_buf: Vec<u8> = format_json_bytes_with_indent(buf);
     let res: Result<(), std::io::Error> = fs::write(path_file, format_buf);
     if res.is_err() {
-        println!("ОШИБКА сохранения конфига");
+        println!("ОШИБКА сохранения");
     }
 }
 
@@ -95,22 +98,56 @@ pub fn file_read(path: String) -> std::io::Result<Vec<u8>> {
 pub fn de_list_json(cfg_path: String) -> Result<ContactList, ()> {
     let res: Result<Vec<u8>, std::io::Error> = file_read(cfg_path);
     if res.is_err() {
-        println!("ОШИБКА чтения конфига");
+        println!("ОШИБКА чтения");
         return Err(());
     }
     let unwrap_res: Result<String, std::string::FromUtf8Error> = String::from_utf8(res.unwrap());
     if unwrap_res.is_err() {
-        println!("Невалидный UTF-8: {:0}",unwrap_res.unwrap_err());
+        println!("Невалидный UTF-8: {:0}", unwrap_res.unwrap_err());
         return Err(());
     }
     let mut res_str: String = unwrap_res.unwrap();
     unsafe {
-        let des: Result<ContactList, simd_json::Error> = simd_json::serde::from_str::<ContactList>(&mut res_str);
+        let des: Result<ContactList, simd_json::Error> =
+            simd_json::serde::from_str::<ContactList>(&mut res_str);
         if des.is_err() {
-            println!("ОШИБКА десерилизации: {:0}",des.unwrap_err());
+            println!("ОШИБКА десерилизации: {:0}", des.unwrap_err());
             return Err(());
         }
         Ok(des.unwrap())
+    }
+}
+
+impl ContactList {
+    pub async fn async_run(&self) -> Result<ContactList, TermuxError> {
+        let mut command = smol_cmd::new("termux-contact-list");
+        let output: Result<std::process::Output, std::io::Error> = command.output().await;
+        match output {
+            Ok(mut output) => {
+                if output.status.success() {
+                    unsafe {
+                        let s: &mut str = std::str::from_utf8_mut(&mut output.stdout).unwrap();
+                        let list: ContactList =
+                            simd_json::serde::from_str::<ContactList>(s).unwrap();
+
+                        return Ok(list);
+                    }
+                }
+                Err(TermuxError::Output(output.to_owned()))
+            }
+            Err(e) => Err(TermuxError::IOError(e)),
+        }
+    }
+    pub async fn async_run_timeout(
+        &self,
+        delay: core::time::Duration,
+    ) -> Result<ContactList, TermuxError> {
+        let mut command: Result<String, std::io::Error> =
+            async_run_api_cmd("termux-contact-list").await;
+        match command {
+            Ok(output) => {}
+            Err(e) => Err(TermuxError::IOError(e)),
+        }
     }
 }
 
--- src/api/run.rs
+++ src/api/run.rs
@@ -1,5 +1,10 @@
+use smol::process::Command as smol_cmd;
 use std::io;
-use std::process::Command;
+use std::process::{Command, Output};
+//use smol::process::unix::*;
+use futures::{select, FutureExt};
+use smol::prelude::Future;
+use smol::Timer;
 
 pub fn run_api_cmd(cmd: &str) -> io::Result<String> {
     let output = Command::new("/data/data/com.termux/files/usr/libexec/termux-api")
@@ -26,4 +31,99 @@ pub fn run_cmd(cmd: &str, arg: &str) -> io::Result<()> {
 pub fn run_cmd_with_args(cmd: &str, args: &[&str]) -> io::Result<()> {
     let _output = Command::new(cmd).args(args).output()?;
     Ok(())
+}
+
+pub fn ret_cmd_arg_async_timeout(cmd: &str, arg: &str) -> impl Future<Output = io::Result<Output>> {
+    let fut = smol_cmd::new(cmd).arg(arg).output();
+    fut
+}
+
+pub fn ret_cmd_async_timeout(cmd: &str) -> impl Future<Output = io::Result<Output>> {
+    let fut = smol_cmd::new(cmd).output();
+    fut
+}
+
+pub fn ret_cmd_with_args_async_timeout(
+    cmd: &str,
+    args: &[&str],
+) -> impl Future<Output = io::Result<Output>> {
+    let fut = smol_cmd::new(cmd).args(args).output();
+    fut
+}
+
+pub enum ResTimeOut {}
+pub enum Res {
+    ResTimeOut,
+    Out(io::Result<Output>),
+}
+async fn run_cmd_time_out(cmd: &str, arg: &str, delay: core::time::Duration) -> Res {
+    let timer: Timer = Timer::after(delay);
+    select! {
+        res = ret_cmd_arg_async_timeout(cmd,arg).fuse() => {return Res::Out(res);},
+        time_out = timer.fuse() => {return Res::ResTimeOut;},
+    }
+}
+
+async fn run_cmd_time_out_not_arg(cmd: &str, delay: core::time::Duration) -> Res {
+    let timer: Timer = Timer::after(delay);
+    select! {
+        res = ret_cmd_async_timeout(cmd).fuse() => {return Res::Out(res);},
+        _time_out = timer.fuse() => {return Res::ResTimeOut;},
+    }
+}
+
+async fn run_cmd_with_args_time_out(cmd: &str, args: &[&str], delay: core::time::Duration) -> Res {
+    let timer: Timer = Timer::after(delay);
+    select! {
+        res = ret_cmd_with_args_async_timeout(cmd,args).fuse() => {return Res::Out(res);},
+        time_out = timer.fuse() => {return Res::ResTimeOut;},
+    }
+}
+
+pub async fn async_run_api_cmd(cmd: &str) -> io::Result<String> {
+    let output = smol_cmd::new("/data/data/com.termux/files/usr/libexec/termux-api")
+        .arg(cmd)
+        .output()
+        .await?;
+
+    Ok(String::from_utf8_lossy(&output.stdout).to_string())
+}
+
+pub async fn async_run_api_cmd_with_args(cmd: &str, args: &[&str]) -> io::Result<String> {
+    let output = smol_cmd::new("/data/data/com.termux/files/usr/libexec/termux-api")
+        .arg(cmd)
+        .args(args)
+        .output()
+        .await?;
+
+    Ok(String::from_utf8_lossy(&output.stdout).to_string())
+}
+
+pub async fn async_run_api_cmd_timeout(
+    cmd: &str,
+    delay: core::time::Duration,
+) -> io::Result<String> {
+    let output: Res =
+        run_cmd_time_out_not_arg("/data/data/com.termux/files/usr/libexec/termux-api", delay).await;
+    if let Res::ResTimeOut = output {}
+    /*
+        .arg(cmd)
+        .output()
+        .await?;
+    */
+    Ok(String::from_utf8_lossy(&output.stdout).to_string())
+}
+
+pub async fn async_run_api_cmd_with_args_tiemout(
+    cmd: &str,
+    args: &[&str],
+    delay: core::time::Duration,
+) -> io::Result<String> {
+    let output = smol_cmd::new("/data/data/com.termux/files/usr/libexec/termux-api")
+        .arg(cmd)
+        .args(args)
+        .output()
+        .await?;
+
+    Ok(String::from_utf8_lossy(&output.stdout).to_string())
 }
